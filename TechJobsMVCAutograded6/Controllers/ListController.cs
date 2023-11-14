605b8b7a141832889d471fa29607854b678db119
emilyalbert <FunkCrusade@outlook.com>
2023-11-13 22:42:23 -0600

    first commit task 2 complete list controller

--- TechJobsMVCAutograded6/Controllers/ListController.cs
+++ TechJobsMVCAutograded6/Controllers/ListController.cs
@@ -44,6 +44,19 @@ public class ListController : Controller
     // TODO #2 - Complete the Jobs action method
     public IActionResult Jobs(string column, string value)
     {
+        //List<Job> jobs;
+
+        //if (column == "all")
+        //{
+        //    jobs = JobData.FindAll();
+        //    ViewBag.title = "All Jobs";
+        //} else
+        //{
+        //    jobs = JobData.FindByColumnAndValue(column, value);
+        //    ViewBag.title = "Jobs from your search";
+        //}
+
+        //ViewBag.jobs = jobs;
         return View();
     }
 }
